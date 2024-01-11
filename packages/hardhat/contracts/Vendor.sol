444a3ac456ea912582818de201e45116e87ff2fd
RH <renhau96@gmail.com>
2024-01-11 05:59:11 -0600

    complete challenge

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,17 +5,64 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	YourToken public yourToken;
+	uint256 public constant tokensPerEth = 100;
 
-  YourToken public yourToken;
+	event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	event SellTokens(
+		address seller,
+		uint256 amountOfTokens,
+		uint256 amountOfETH
+	);
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+	}
 
-  // ToDo: create a payable buyTokens() function:
+	fallback() external payable {}
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+	receive() external payable {}
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+	function buyTokens() public payable {
+		address sender = msg.sender;
+		uint256 amountOfTokens = msg.value * tokensPerEth;
+		require(msg.value > 0, "Insufficient ETH to Purchase BGB");
+		yourToken.transfer(sender, amountOfTokens);
+		emit BuyTokens(sender, msg.value, amountOfTokens);
+	}
+
+	// ToDo: create a withdraw() function that lets the owner withdraw ETH
+	function withdraw() public payable onlyOwner {
+		(bool success, ) = msg.sender.call{ value: address(this).balance }("");
+		require(success, "Not Owner!");
+	}
+
+	// ToDo: create a sellTokens(uint256 _amount) function:
+	function sellTokens(uint256 amount) public {
+		uint256 amountOfEthForSeller = amount / tokensPerEth;
+
+		// check to prevent negative or zero values
+		require(amount > 0, "Please enter amount more than 0");
+
+		// check to ensure seller has enough tokens to sell
+		require(
+			yourToken.balanceOf(msg.sender) >= amount,
+			"Insufficient balance to sell"
+		);
+
+		// check to ensure vendor has enough tokens to sell
+		require(
+			address(this).balance >= amountOfEthForSeller,
+			"Insufficient liquidity in Vendor to perform sale"
+		);
+
+		bool success = yourToken.transferFrom(
+			msg.sender,
+			address(this),
+			amount
+		);
+		require(success, "Failed to transfer token to Vendor");
+		payable(msg.sender).transfer(amountOfEthForSeller);
+		emit SellTokens(address(this), amount, amountOfEthForSeller);
+	}
 }
